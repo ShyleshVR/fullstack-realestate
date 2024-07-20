12ed8acd069604327cf128b856508f4f3ca9ef35
Shylesh <98650862+ShyleshVR@users.noreply.github.com>
2024-07-20 09:55:07 +0530

    Added about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='mt-10 p-5'>
+      <h1 className='text-slate-700 text-3xl font-bold mb-2'>About <span className='text-slate-500'> Arunachala Constructions </span></h1>
+      <p>At Arunachala Constructions, we pride ourselves on being a leading construction company based in Ranipet, specializing in both residential and industrial projects. With years of experience and a commitment to excellence, we have built a reputation for delivering high-quality construction solutions tailored to meet the unique needs of our clients. </p>
+      <br />
+      <h1 className='text-slate-700 text-3xl font-bold mb-2'>Our Story</h1>
+      <p>Founded with a vision to transform the construction landscape in Ranipet, Arunachala Constructions has grown into a trusted name in the industry. Our journey began with a passion for building and a dedication to quality craftsmanship. Over the years, we have completed numerous projects, each reflecting our unwavering commitment to innovation, integrity, and customer satisfaction.</p>
+      <br  />
+      <h1 className='text-slate-700 text-3xl font-bold mb-2'>What We Do</h1>
+      <p><strong>Residential Construction:</strong> We understand that a home is more than just a place to live; it's a sanctuary where memories are made. Our team works closely with clients to design and build homes that are not only aesthetically pleasing but also functional and durable. From cozy apartments to luxurious villas, we cater to a wide range of residential needs.</p>
+      <br />
+      <p><strong>Industrial Construction:</strong> Our expertise extends to the industrial sector, where we undertake the construction of factories, warehouses, and other industrial facilities. We focus on delivering projects that enhance operational efficiency and meet the specific requirements of our industrial clients. Our approach is centered on safety, sustainability, and cutting-edge technology.</p>
+      <br />
+      <h1 className='text-slate-700 text-3xl font-bold mb-2'>Contact Us</h1>
+      <p>Ready to bring your construction project to life? Get in touch with us today to discuss your residential or industrial construction needs. We look forward to partnering with you and turning your vision into reality.</p>
+    </div>
   )
 }
